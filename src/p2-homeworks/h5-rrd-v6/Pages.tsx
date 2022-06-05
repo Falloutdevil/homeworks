96adfeb288611ffc8e5490aff6b16118f0785d4e
Falloutdevil <Falloutdevil@ya.ru>
2022-06-05 22:57:24 +0300

    hw5 404

--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import {Navigate, Route, Routes} from 'react-router-dom';
 import PreJunior from './pages/PreJunior';
 import Error404 from './pages/Error404';
-import Junior from './pages/Junior';
-import JuniorPlus from './pages/JuniorPlus';
+
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -23,8 +22,8 @@ function Pages() {
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                {/*<Route path={PATH.JUNIOR} element={<Junior/>}/>*/}
+                {/*<Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>*/}
 
                 {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
                 <Route path={'/*'} element={<Error404/>}/>
